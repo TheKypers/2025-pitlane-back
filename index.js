c192b4ad4ef2759d862f2eaf02109af504e26606
Martina-Catania <tinicatania1111@gmail.com>
2025-09-14 19:46:48 -0300

    added cors to allow frontend requests

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3005;
 
-
 const routes = require('./routes');
+
+// Configure CORS to allow requests from the frontend
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 
 app.use(express.json());
 app.use('/', routes);
